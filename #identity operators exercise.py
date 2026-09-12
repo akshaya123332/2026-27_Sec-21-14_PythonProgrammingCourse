cf2cebc34a11d69cd11599049017ad0c43341d28
akshaya123332 <jampanaakshaya02@gmail.com>
2026-09-12 17:30:07 +0530

    Add files via upload

---
+++ #identity operators exercise.py
@@ -0,0 +1,10 @@
+
+
+a=[1,2,3]
+b=a 
+result=b is a #is membership operator
+print("result of ",b, "is not",a,"is :",result)
+
+a=[1,2,3]
+result=b is a # is membership operator
+print("result of",b, "is not ",a,"is :",result)
