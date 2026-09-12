f719a249456903a9af0acc99a752dc6f4db09772
akshaya123332 <jampanaakshaya02@gmail.com>
2026-09-12 17:24:00 +0530

    Add files via upload

---
+++ swapping_two_numbers.py
@@ -0,0 +1,14 @@
+# Program to swap two numbers without using a third variable
+
+# Program to swap two numbers without using a third variable
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+a = a + b
+b = a - b
+a = a - b
+
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
