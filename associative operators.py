badde681113d9864082d3b1567ad1546ac643763
akshaya123332 <jampanaakshaya02@gmail.com>
2026-09-12 17:31:51 +0530

    Add files via upload

---
+++ associative operators.py
@@ -0,0 +1,34 @@
+a=10
+b=3
+print(f"the result of {a}/{b} is : {a/b}")
+print(f"the result of {a}//{b} is : {a//b}")
+print("***********************************************")
+a=18
+b=3
+result=a*b//2
+print(f"the result of {a}*{b}//2 is : {result}")
+print("***********************************************")
+a=100
+result=a//10//2
+print(f"the result of {a}//10//2 is : {result}")
+print("***********************************************")
+a=2
+b=3
+result=a+b*2**2
+print(f"the result of {a}+{b}*2**2 is {result}")
+print("***********************************************")
+a=5
+b=3
+result=(a+b)*2
+print(f"the result of ({a}+{b})*2 is : {result}")
+print("***********************************************")
+a=5 
+b=3
+result=a+b*2>10
+print(f"the result of {a}+{b}*2>10 is : {result}")
+print("***********************************************")
+a=5 
+b=3
+result=a>b and b>1 or a<0
+print(f"the result of {a}>{b} and {b}>1 or {a}<0 is {result}")
+print("***********************************************")
